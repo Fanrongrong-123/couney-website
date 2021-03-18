62979681c89f0a4888e8673d83bfcc29e051a9fb
Fanrr <2514509399@qq.com>
2021-03-18 11:41:37 +0800

    deploy

---
+++ precache-manifest.977541ec4bb8b20564fa76b1b83dfb7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3b0b1a6807994e76dc",
+    "url": "/couney-website/css/app.82a4c790.css"
+  },
+  {
+    "revision": "5d997f196d43c3877bf7dd9cb51b84e9",
+    "url": "/couney-website/index.html"
+  },
+  {
+    "revision": "dc3b0b1a6807994e76dc",
+    "url": "/couney-website/js/app.8377a53b.js"
+  },
+  {
+    "revision": "0ba0648d5acfd538ac80",
+    "url": "/couney-website/js/chunk-vendors.2fe539a9.js"
+  },
+  {
+    "revision": "21e7b5475cbf76579fc649f422155d47",
+    "url": "/couney-website/manifest.json"
+  },
+  {
+    "revision": "9cdf8089ff7576e39bd3aa8864ce78c0",
+    "url": "/couney-website/qr.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/couney-website/robots.txt"
+  }
+]);
